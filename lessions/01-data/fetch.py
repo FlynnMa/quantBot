009cdbbc74d0ff925a6c1f1218ec9f24b3699d82
Flynn <shmayunfei@qq.com>
2021-08-14 20:11:45 +0800

    fix fetch typo

--- lessions/01-data/fetch.py
+++ lessions/01-data/fetch.py
@@ -1,13 +1,14 @@
+import os
 import matplotlib.pyplot as plt
 import pandas as pd
 import pandas_datareader as pdr
 
 
-df = pdr.get_data_tiingo('AAPL', start='2021-01-01', end='2021-08-05', api_key=os.getenv('TIINGO_API_KEY'))
+df = pdr.get_data_tiingo('601398', start='2021-01-01', end='2021-08-05', api_key=os.getenv('TIINGO_API_KEY'))
 
 
 fig = plt.figure(figsize=(20, 12))
-fig.suptitle("volume plotting")
+fig.suptitle("price plotting")
 df['adjClose'].plot(rot=90, grid=True)
-fig.savefig("volume.png")
+fig.savefig("price.png")
 
